51393b0563003df7897aafb31ed1571a0fa61ef0
fan1296 <165129265+fan1235@users.noreply.github.com>
2024-10-10 23:42:40 +0800

    I received the first three API codes from Li Nuoen

---
+++ Letian YuA2-clientside/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+
+// to parse URL-encoded and JSON data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// to serve static files
+app.use(express.static(__dirname));
+
+// route to serve index.html
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// route to serve concert.html
+app.get("/concert", (req, res) => {
+    res.sendFile(path.join(__dirname, "concert.html"));
+});
+
+// route to serve add_concert.html
+app.get("/add_concert", (req, res) => {
+    res.sendFile(path.join(__dirname, "add_concert.html"));
+});
+
+// route to serve fundraiser.html (pointing to add_concert.html)
+app.get("/fundraiser.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "add_concert.html"));
+});
+
+// Start the server on port 8080
+app.listen(8080, () => {
+    console.log("Server running on port 8080");
+});
+
+
+
+
